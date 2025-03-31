b0c534a63b51ed8e51a7768f957b08dfe5efffbc
Sachintha <chamikara24sachintha@gmail.com>
2025-03-31 21:32:39 +0530

    I add to some development for my project

--- index.php
+++ index.php
@@ -14,30 +14,58 @@
         src: url(GravitasOne-Regular.ttf) format('truetype');
     }
 
+    @font-face {
+        font-family: 'Dancing Script';
+        src: url(DancingScript-VariableFont_wght.ttf) format('truetype');
+    }
+
     #wrapper {
-        max-width: 800px;
-        min-height: 500px;
+        max-width: 1000px;
+        min-height: 1000px;
         display: flex;
         background-color: rgb(225, 114, 114);
         margin: 0 auto;
         color: white;
+        font-family: 'Dancing Script', cursive;
+        font-size: 14px;
     }
 
     #left_pannel {
-        min-height: 400px;
-        background-color: rgb(192, 26, 26);
+        min-height: 100%;
+        background-color: rgb(188, 84, 84);
         flex: 1;
+        padding: 10px;
+        text-align: center;
+    }
+
+    #left_pannel img {
+        width: 50%;
+        border: soiid thin black;
+        border-radius: 25%;
+        margin: 10px;
+        padding: 10px;
+    }
+
+    #left_pannel label {
+        width: 100%;
+        height: 30px;
+        display: block;
+        font-size: 14px;
+        background-color: #404b56;
+        border-bottom: solid thin white;
+        cursor: pointer;
     }
 
     #right_pannel {
-        min-height: 400px;
-        background-color: rgb(15, 15, 232);
+        min-height: 100%;
+        background-color: rgb(232, 232, 243);
         flex: 5;
+        text-align: center;
     }
 
     #header {
         height: 50px;
-        background-color: rgb(51, 20, 227);
+        background-color: rgb(167, 156, 234);
         font-size: 40px;
         text-align: center;
         font-family: Gravitas One, sans-serif;
@@ -50,8 +78,8 @@
     }
 
     #inner_left_pannel {
-        width: 20%;
-        background-color: rgb(213, 130, 21);
+        width: 50%;
+        background-color: rgb(203, 192, 178);
     }
 
     #inner_right_pannel {
@@ -66,12 +94,28 @@
 <body>
     <div id="wrapper">
         <div id="left_pannel">
-
+            <div style="padding: 10px;">
+                <img src="./sachintha passport size.jpg">
+                <br>
+                Sachintha chamikara
+                <br>
+                <span style="font-size: 12px;opacity: 0.5p;"> chamikara24sachintha@gmail.com</span>
+                <div>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <label for="box">Chat</label>
+                    <label>Contacts</label>
+                    <label>Setting</label>
+                </div>
+            </div>
         </div>
         <div id="right_pannel">
             <div id="header">My Chat</div>
             <div id="container" style="display: flex;">
                 <div id="inner_left_pannel"></div>
+                
                 <div id="inner_right_pannel"></div>
             </div>
         </div>
