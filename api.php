18b8f9957488d1bc53e4479e2201302d8d95c57d
Sachintha <chamikara24sachintha@gmail.com>
2025-04-07 22:10:55 +0530

    stop the part 14  mi 14.46

--- api.php
+++ api.php
@@ -1,10 +1,27 @@
- <?php
+<?php
 
 require_once("classes/autoload.php");
 
-$DB = new database();
+$DB = new Database();
 
 $DATA_RAW = file_get_contents("php://input");
-$DATA_OBJ = json_decode($data);
+$DATA_OBJ = json_decode($DATA_RAW);
 
-//process the data
+if (isset($DATA_OBJ->data_type) && $DATA_OBJ->data_type == "signup") {
+
+    $data = [];
+    $data['userid'] = $DB->generate_id(20);
+    $data['username'] = $DATA_OBJ->username ?? '';
+    $data['email'] = $DATA_OBJ->email ?? '';
+    $data['password'] = password_hash($DATA_OBJ->password ?? '', PASSWORD_DEFAULT);
+    $data['data'] = date("Y-m-d H:i:s");
+
+    $query = "INSERT INTO users (userid, username, email, password, data) VALUES (:userid, :username, :email, :password, :data)";
+    $result = $DB->write($query, $data);
+
+    if ($result) {
+        echo "Your profile was created.";
+    } else {
+        echo "Your profile was NOT created.";
+    }
+}
--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 
-class database
+class Database
 {
     private $conn;
 
@@ -8,34 +8,41 @@ class database
     {
         $this->conn = $this->connect();
     }
+
     private function connect()
     {
-        $string = "mysql:host=localhost;gitmychat_db";
+        $string = "mysql:host=localhost;dbname=gitmychat_db";
 
         try {
             $connection = new PDO($string, DBUSER, DBPASS);
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $connection;
         } catch (PDOException $e) {
-
             echo $e->getMessage();
             die;
         }
-        return false;
     }
-    public function write($query,$data_array =[]){
-        $con = $this->connect();
 
+    public function write($query, $data_array = [])
+    {
+        $con = $this->connect();
         $statement = $con->prepare($query);
 
-    foreach ($data_array as $key => $value){
-          # code.......    
-        $statement->bindparam(':'.$key,$value);
+        foreach ($data_array as $key => $value) {
+            $statement->bindValue(':' . $key, $value);
         }
 
-       $check = $statement->execute();
-       if($check){
-          return true; 
-       }
-       return false;
+        $check = $statement->execute();
+        return $check ? true : false;
+    }
+
+    public function generate_id($max)
+    {
+        $rand = "";
+        $rand_count = rand(4, $max);
+        for ($i = 0; $i < $rand_count; $i++) {
+            $rand .= rand(0, 9);
+        }
+        return $rand;
     }
 }
